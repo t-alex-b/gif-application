d973748a244c8944143d6a897dabe996d46fe2a0
t-alex-b <sunnoperatic@aol.com>
2018-02-27 18:21:26 -0700

    progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+
+	var gifCats = [Camels, Snorlax, Pumpkins, Elevators];
+	
+	
+	function createBtns() {
+
+		$('#categoryBtns').empty();
+		for (var i = 0; i < gifCats.length; i++) {
+			var button = $('<button>');
+			
+		}
+	}
+
+
+
+
+
+
+
+}
